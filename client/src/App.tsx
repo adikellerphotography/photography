6ab47fe76ff513fe9f99838514cd5db9316b3513
kellersn <38783902-kellersn@users.noreply.replit.com>
2025-01-12 18:42:11 +0000

    Agent query: Is the navigation between pages working correctly?
    Remove page transitions and simplify routing.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/d0876a58-5324-4669-b4cd-c8a2304e4963/b4162f01-0bb7-4ef8-a12b-4aa77bd02bf5.jpg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,8 @@
-import { Switch, Route, useLocation } from "wouter";
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "@/hooks/use-theme";
 import { LanguageProvider } from "@/hooks/use-language";
-import { AnimatePresence } from "framer-motion";
 import Navbar from "@/components/Navbar";
 import Home from "@/pages/home";
 import Gallery from "@/pages/Gallery";
@@ -11,46 +10,19 @@ import About from "@/pages/About";
 import Pricing from "@/pages/Pricing";
 import NotFound from "@/pages/not-found";
 import { Toaster } from "@/components/ui/toaster";
-import PageTransition from "@/components/PageTransition";
 
 function Router() {
-  const [location] = useLocation();
-
   return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar className="z-50" />
-      <div className="flex-1 relative">
-        <AnimatePresence mode="wait" initial={false}>
-          <Switch key={location}>
-            <Route path="/">
-              <PageTransition>
-                <Home />
-              </PageTransition>
-            </Route>
-            <Route path="/gallery">
-              <PageTransition>
-                <Gallery />
-              </PageTransition>
-            </Route>
-            <Route path="/about">
-              <PageTransition>
-                <About />
-              </PageTransition>
-            </Route>
-            <Route path="/pricing">
-              <PageTransition>
-                <Pricing />
-              </PageTransition>
-            </Route>
-            <Route>
-              <PageTransition>
-                <NotFound />
-              </PageTransition>
-            </Route>
-          </Switch>
-        </AnimatePresence>
-      </div>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/gallery" component={Gallery} />
+        <Route path="/about" component={About} />
+        <Route path="/pricing" component={Pricing} />
+        <Route component={NotFound} />
+      </Switch>
+    </>
   );
 }
 
