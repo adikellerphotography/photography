ae7e70ffd9ebb36ee6910087fd735715a48492df
Adi Keller <38783902-kellersn@users.noreply.replit.com>
2025-01-19 18:09:43 +0000

    Restored to '1bcb63602cd013034cbdf8148c04873e12f77c89'
    Replit-Restored-To:1bcb63602cd013034cbdf8148c04873e12f77c89

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -66,7 +66,7 @@ export default function Navbar() {
               <LanguageToggle />
               <ThemeToggle />
             </div>
-            <div className="text-sm font-cormorant tracking-wide text-foreground/70 text-center absolute w-full px-12">
+            <div className="text-lg font-semibold text-foreground/70"> {/*Opacity adjusted here */}
               Adi Keller Photography
             </div>
 
